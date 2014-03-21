411f467834c6c458b549e9bcef5b154e520c7188
erlaroche <E.R.Laroche@gmail.com>
2014-03-20 23:00:09 -0500

    Run migrations to create three seperate tables with name for column heading

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321032404) do
+ActiveRecord::Schema.define(version: 20140321035759) do
 
   create_table "composers", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "editions", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "works", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
