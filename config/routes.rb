0eed2b6895845a841f32047b2dd8a9d16a9e835e
erlaroche <E.R.Laroche@gmail.com>
2014-03-20 22:44:18 -0500

    Create controller main with method welcome that look for all composers(stored in instance variable @composer)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 R4rmusic1::Application.routes.draw do
+  get "main/welcome"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
