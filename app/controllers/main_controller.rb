665b7a57621d0e0c5b9704bbce1c78b6b13cfb05
erlaroche <E.R.Laroche@gmail.com>
2014-03-21 12:03:31 -0500

    Seed databases

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def welcome
-    @composers = Composer.find(:all).sort_by {|c| [c.last_name, c.first_name] }
+    @composers = Composer.find(:all).sort_by {|c| [c.name] }
   end
 end
