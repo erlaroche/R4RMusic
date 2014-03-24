fae6f908c331c5c0c008ef502dbd141a054bf1dd
erlaroche <E.R.Laroche@gmail.com>
2014-03-23 23:39:29 -0500

    Add working composer/show view

--- app/controllers/composer_controller.rb
+++ app/controllers/composer_controller.rb
@@ -1,5 +1,6 @@
 class ComposerController < ApplicationController
   def show
-    @composer = Composer.first.name
+    @composers = Composer.find(:all).sort_by {|c| [c.name] }
+    @works = Work.find(:all).sort_by {|w| [w.composer_id] }
   end
 end
